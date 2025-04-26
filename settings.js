2e6b443c5566fab238ea83447ea29d1ef8a79f10
sonugupta180025 <116357623+sonugupta180025@users.noreply.github.com>
2025-04-26 23:35:20 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '91702395151X', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'SonuGupta', // Your name
+  ownerNumber: '917379152368X', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
